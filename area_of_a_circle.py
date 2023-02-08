10b0a4edd58981ae3cc8279aa4508f0b2dc8dbc5
kareemcha <k.cha.esc@gmail.com>
2023-02-08 13:30:06 -0600

    added a new radius input and expanded pi

--- area_of_a_circle.py
+++ area_of_a_circle.py
@@ -1,5 +1,5 @@
 def compute_area_of_circle(radius):
-	pi = 3.14
+	pi = 3.1415
 	area = pi * radius * radius
 	return area
 
@@ -10,3 +10,8 @@ print(f"The area of circle with radius {radius1} is: {area1}")
 radius2 = 40
 area2 = compute_area_of_circle(radius2)
 print(f"The area of circle with radius {radius2} is: {area2}")
+
+radius3 = 5
+area3 = compute_area_of_circle(radius3)
+print(f"The area of circle with radius {radius3} is: {area3}")
+
